92140e1bae084f8e7bd57943f260a1592420fc04
Ross <rossmylesobrien@gmail.com>
2019-10-15 20:44:23 +0100

    Adding a range for a function

--- 2.go
+++ 2.go
@@ -1,1 +1,22 @@
-package functions
+package main
+
+import "fmt"
+
+func main() {
+
+	foo(1,2,3,4,5,6,7)
+
+}
+
+
+func foo(x ...int){
+
+	fmt.Println(x)
+
+	sum := 0
+
+	for i, v := range x{
+		sum += v
+		fmt.Println("The index position,",i, " with the value:",v, "and the sum is: ",sum)
+	}
+}
